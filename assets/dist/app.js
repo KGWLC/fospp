7e77b268f67d6cea35fe5c7b592c82f3fb54b9df
Karl Weber <93593968+KGWLC@users.noreply.github.com>
2024-04-29 23:24:14 +0200

    Create app.js

---
+++ assets/dist/app.js
@@ -0,0 +1,7 @@
+// This function changes the background color of the body to red
+function logToConsole() {
+  console.log('Hiiiii');
+}
+
+// Add event listener for DOMContentLoaded to run the function after the page loads
+document.addEventListener("DOMContentLoaded", logToConsole);
