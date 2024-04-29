6b9b2a9c7752778eda33b0f06d705108503c1ac8
Karl Weber <93593968+KGWLC@users.noreply.github.com>
2024-04-29 22:25:57 +0200

    Update and rename index.js to assets/dev/app.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// This function changes the background color of the body to red
-function changeBackgroundColor() {
-  document.body.style.backgroundColor = "red";
-  console.log("background changed to red");
-}
-
-// Add event listener for DOMContentLoaded to run the function after the page loads
-document.addEventListener("DOMContentLoaded", changeBackgroundColor);
