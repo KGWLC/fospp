bbe8165fcd7e4818375cc36cbc428b20eed771e8
Karl Weber <93593968+KGWLC@users.noreply.github.com>
2024-04-30 11:11:52 +0200

    Update app.js

--- assets/dist/app.js
+++ assets/dist/app.js
@@ -1,3 +1,11 @@
+const cssURL = "https://cdn.jsdelivr.net/gh/KGWLC/fospp@prod/assets/dist/app.css";
+const currentDate = new Date().toISOString().slice(0,10).replace(/-/g, "");
+const updatedURL = cssURL + "?update=" + currentDate;
+
+// Example of how to set it in an HTML link element dynamically
+document.getElementById("myCSS").href = updatedURL;
+
+
 // This function changes the background color of the body to red
 function logToConsole() {
   console.log('Hiiiii');
