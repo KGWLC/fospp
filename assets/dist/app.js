7c1bf3f4171b59d5a7e5e7f5b4a1582d6db0870c
Karl Weber <93593968+KGWLC@users.noreply.github.com>
2024-04-30 11:20:33 +0200

    Update and rename app.js to home.js

--- assets/dist/app.js
+++
@@ -1,7 +0,0 @@
-// This function changes the background color of the body to red
-function logToConsole() {
-  console.log('Hiiiii');
-}
-
-// Add event listener for DOMContentLoaded to run the function after the page loads
-document.addEventListener("DOMContentLoaded", logToConsole);
